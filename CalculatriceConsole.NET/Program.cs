7f44acdab3016983f8d9fec491f76d47e55929f2
Sufyān <132440690+Sofiane5900@users.noreply.github.com>
2024-12-19 16:42:30 +0100

    ajout du prompt

--- CalculatriceConsole.NET/Program.cs
+++ CalculatriceConsole.NET/Program.cs
@@ -4,47 +4,33 @@ using Spectre.Console.Cli;
 // Valeur de base
 List<double> listNombres = new List<double>();
 
-
 do
 {
-            AnsiConsole.Write(
-         new FigletText("Calculatrice.NET")
-             .Color(Color.Red));
-;
-    AnsiConsole.MarkupLine("[bold red] -- Menu --[/]");
-    Console.WriteLine("1 -- Saisie des nombres");
-    Console.WriteLine("2 -- Addition");
-    Console.WriteLine("3 -- Soustraction");
-    Console.WriteLine("4 -- Multiplication");
-    Console.WriteLine("5 -- Division");
-    Console.WriteLine("0 -- Quitter");
+    AnsiConsole.Write(
+        new FigletText("Calculatrice.NET")
+            .Color(Color.Red));
 
-    Console.Write("Faites un choix : ");
-    int choice;
-    bool successChoice = int.TryParse(Console.ReadLine(), out choice);
+    var choix = AnsiConsole.Prompt(
+        new SelectionPrompt<string>()
+            .Title("Quel option [green]choisissez-vous[/]?")
+            .PageSize(10)
+            .MoreChoicesText("[grey](Utilisez les flèches pour naviguer)[/]")
+            .AddChoices(new[] {
+                "Saisie des nombres", "Addition", "Soustraction",
+                "Multiplication", "Division", "Quitter"
+            }));
 
-
-    if (!successChoice)
+    switch (choix)
     {
-        Console.Clear();
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine("\tVous avez utiliser un caractère non autorisé.");
-        Console.ResetColor();
-        continue;
-    }
-
-
-    switch (choice)
-    {
-        case 0: // Quitter l'application
+        case "Quitter": // Quitter l'application
             Environment.Exit(0);
             break;
-        case 1: // Saisie des nombres
+        case "Saisie des nombres": // Saisie des nombres
             Console.Clear();
-             int compteur = 0;
+            int compteur = 0;
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("--- Saisir les notes ---");
-            Console.WriteLine("(999 pour stoper la saisie) \n");
+            Console.WriteLine("(999 pour stopper la saisie) \n");
             Console.ResetColor();
             while (true)
             {
@@ -56,25 +42,25 @@ do
                 {
                     AnsiConsole.MarkupLine("\t\t[red underline]Erreur de saisie, veuillez saisir un nombre.[/]");
                 }
-                else if (nombresInput.Equals(999)) // Si la saisie de l'user = 999 je sors de ma "case 1"
+                else if (nombresInput.Equals(999)) // Si la saisie de l'utilisateur = 999 je sors de ma "case 1"
                 {
                     Console.Clear();
                     break;
                 }
-                else // Si la saisie de l'user != 999, alors j'ajoute sa saisie a ma list et j'incrémente compteur
+                else // Si la saisie de l'utilisateur != 999, alors j'ajoute sa saisie à ma liste et j'incrémente compteur
                 {
                     listNombres.Add(nombresInput);
                     compteur++;
                 }
             }
             break;
-        case 2: // Addition
+        case "Addition": // Addition
             if (listNombres.Count == 0)
             {
                 Console.Clear();
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Vous n'avez pas saisie de nombre.");
-                Console.ResetColor ();
+                Console.WriteLine("Vous n'avez pas saisi de nombre.");
+                Console.ResetColor();
                 break;
             }
             else
@@ -82,19 +68,18 @@ do
                 Console.Clear();
                 double somme = listNombres.Sum();
                 Console.ForegroundColor = ConsoleColor.Blue;
-                Console.WriteLine($"La somme des nombres que vous avez saisie est : {somme}");
+                Console.WriteLine($"La somme des nombres que vous avez saisis est : {somme}");
                 Console.ResetColor();
                 listNombres.Clear();
-
             }
             break;
-        case 3: // Soustraction
+        case "Soustraction": // Soustraction
             if (listNombres.Count == 0)
             {
                 Console.Clear();
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Vous n'avez pas saisie de nombre.");
-                Console.ResetColor () ;
+                Console.WriteLine("Vous n'avez pas saisi de nombre.");
+                Console.ResetColor();
                 break;
             }
             else
@@ -102,17 +87,17 @@ do
                 double difference = listNombres.Aggregate((a, b) => a - b);
                 Console.Clear();
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine($"La différence des nombres que vous avez saisie est : {difference}");
+                Console.WriteLine($"La différence des nombres que vous avez saisis est : {difference}");
                 listNombres.Clear();
                 Console.ResetColor();
             }
             break;
-        case 4: // Multiplication
+        case "Multiplication": // Multiplication
             if (listNombres.Count == 0)
             {
                 Console.Clear();
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Vous n'avez pas saisie de nombre.");
+                Console.WriteLine("Vous n'avez pas saisi de nombre.");
                 Console.ResetColor();
                 break;
             }
@@ -121,17 +106,17 @@ do
                 double multiplicateur = listNombres.Aggregate((a, b) => a * b);
                 Console.Clear();
                 Console.ForegroundColor = ConsoleColor.Blue;
-                Console.WriteLine($"La multiplication des nombres que vous avez saisie est : {multiplicateur}");
+                Console.WriteLine($"La multiplication des nombres que vous avez saisis est : {multiplicateur}");
                 listNombres.Clear();
                 Console.ResetColor();
             }
             break;
-        case 5: // Division
+        case "Division": // Division
             if (listNombres.Count == 0)
             {
                 Console.Clear();
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Vous n'avez pas saisie de nombre.");
+                Console.WriteLine("Vous n'avez pas saisi de nombre.");
                 Console.ResetColor();
                 break;
             }
@@ -140,15 +125,15 @@ do
                 double division = listNombres.Aggregate((a, b) => a / b);
                 Console.Clear();
                 Console.ForegroundColor = ConsoleColor.Magenta;
-                Console.WriteLine($"La division des nombres que vous avez saisie est : {division}");
+                Console.WriteLine($"La division des nombres que vous avez saisis est : {division}");
                 listNombres.Clear();
                 Console.ResetColor();
             }
             break;
-        default: // Tout ce qui n'éxiste pas en tant que case, valeur de défault
+        default: // Tout ce qui n'existe pas en tant que case, valeur de défaut
             Console.Clear();
             Console.ForegroundColor = ConsoleColor.Red;
-            AnsiConsole.MarkupLine("[red underline]Vous n'étes pas autorisé a effectué cette action !.[/]");
+            AnsiConsole.MarkupLine("[red underline]Vous n'êtes pas autorisé à effectuer cette action ![/]");
             Console.ResetColor();
             break;
     }
