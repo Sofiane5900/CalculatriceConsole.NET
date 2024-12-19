446f97804fcd6956db3fcabb6617202c6d0ab924
Sufyān <132440690+Sofiane5900@users.noreply.github.com>
2024-12-19 14:19:02 +0100

    case 1 ajouté

--- CalculatriceConsole.NET/Program.cs
+++ CalculatriceConsole.NET/Program.cs
@@ -1,10 +1,58 @@
 ﻿
 // Valeur de base
-int nombreUn;
-int nombreDeux;
+List<int> listNombres = new List<int>();
 
 
+do
+{
 
-Console.WriteLine("--- Menu calculatrice ----");
+    Console.WriteLine("--- Menu calculatrice ----");
 
-Console.WriteLine("Choisisez un option");
+    Console.WriteLine("1 -- Saisie des nombres");
+    Console.WriteLine("2 -- Addition");
+    Console.WriteLine("3 -- Soustraction");
+    Console.WriteLine("4 -- Multiplication");
+    Console.WriteLine("5 -- Division");
+    Console.WriteLine("0 -- Quitter");
+
+    Console.Write("Faites un choix : ");
+    int choice = int.Parse(Console.ReadLine());
+    switch (choice)
+    {
+        case 0:
+            Environment.Exit(0);
+            break;
+        case 1: // Saisie des nombres
+            Console.Clear();
+             int compteur = 0;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("--- Saisir les notes ---");
+            Console.WriteLine("(999 pour stoper la saisie) \n");
+            Console.ResetColor();
+            while (true)
+            {
+                Console.Write($"Merci de saisir le nombre {compteur + 1}: ");
+                int nombresInput;
+                bool success = int.TryParse(Console.ReadLine(), out nombresInput);
+
+                if (!success)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\t\tErreur de saisie, veuillez saisir un nombre.");
+                    Console.ResetColor();
+                }
+                else if (nombresInput.Equals(999)) // Si la saisie de l'user = 999 je sors de ma "case 1"
+                {
+                    Console.Clear();
+                    break;
+                }
+                else // Si la saisie de l'user != 999, alors j'ajoute sa saisie a ma list et j'incrémente compteur
+                {
+                    listNombres.Add(nombresInput);
+                    compteur++;
+                }
+            }
+            break;
+        case 2
+    }
+} while (true);
